21e35aa999b95600dc345bb7b6ab165dee8dcb24
jizya0208 <moridyan606@gmail.com>
2022-02-16 13:02:53 +0900

    a

--- page-contact.php
+++ page-contact.php
@@ -1,3 +1,20 @@
+<?php
+/*
+Template Name: お問い合わせ
+*/
+?>
 <?php get_header(); ?>
-   [mwform_formkey key="27"]
+   <section class="contact-form">
+      <div class="">
+         <h1 class="main-heading">CONTACT</h1>
+         <p class="contact-content">テキストテキストテキストテキスト「<a href="/privacy-policy.html"
+            class="privacy-policy">個人情報保護方針</a>」テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+         <div>
+         <?php
+               $post_id = 25;
+               $post = get_post($post_id);
+               echo apply_filters( 'the_content', $page->post_content );
+         ?>
+      </div>
+  </section>
 <?php get_footer(); ?>
