bc5288a13adad5a0295f94fa2f9ee8586a9c0efc
jizya0208 <moridyan606@gmail.com>
2022-02-16 13:20:25 +0900

    the_content

--- page-contact.php
+++ page-contact.php
@@ -10,11 +10,17 @@ Template Name: お問い合わせ
          <p class="contact-content">テキストテキストテキストテキスト「<a href="/privacy-policy.html"
             class="privacy-policy">個人情報保護方針</a>」テキストテキストテキストテキストテキストテキストテキストテキスト</p>
          <div>
-         <?php
-               $post_id = 25;
+               <!-- $post_id = 25;
                $post = get_post($post_id);
-               echo apply_filters( 'the_content', $page->post_content );
-         ?>
+               echo apply_filters( 'the_content', $page->post_content ); -->
+         <?php if (have_posts()): ?>
+            <?php while (have_posts()) : the_post(); ?>
+               <?php echo apply_filters(the_content()); ?>
+               <?php echo apply_filters( 'the_content', get_post(25)->post_content ); ?>
+            <?php endwhile; ?>
+         <?php else: ?>
+           投稿が無い時の処理
+         <?php endif; ?>
       </div>
   </section>
 <?php get_footer(); ?>
