fc623f98e02823006a7d8e614484c92c74366339
jizya0208 <moridyan606@gmail.com>
2022-02-18 09:53:11 +0900

    contactフォーム

--- page-contact.php
+++ page-contact.php
@@ -5,22 +5,22 @@ Template Name: お問い合わせ
 ?>
 <?php get_header(); ?>
    <section class="contact-form">
-      <div class="">
-         <h1 class="main-heading">CONTACT</h1>
-         <p class="contact-content">テキストテキストテキストテキスト「<a href="/privacy-policy.html"
-            class="privacy-policy">個人情報保護方針</a>」テキストテキストテキストテキストテキストテキストテキストテキスト</p>
-         <div>
+      <div class="p-contact">
+         <h1 class="c-ttl">CONTACT</h1>
+         <p class="text">お仕事のご依頼などお待ちしております。</p>
+         <div class="form-box">
+            <?php if (have_posts()): ?>
+               <?php while (have_posts()) : the_post(); ?>
+                  <?php echo apply_filters(the_content()) ?>
+                  <?php echo apply_filters( 'the_content', get_post(25)->post_content ); ?>
+               <?php endwhile; ?>
+            <?php else: ?>
+               投稿が無い時の処理
+            <?php endif; ?>
+         </div>
                <!-- $post_id = 25;
                $post = get_post($post_id);
                echo apply_filters( 'the_content', $page->post_content ); -->
-         <?php if (have_posts()): ?>
-            <?php while (have_posts()) : the_post(); ?>
-               <?php echo apply_filters(the_content()); ?>
-               <?php echo apply_filters( 'the_content', get_post(25)->post_content ); ?>
-            <?php endwhile; ?>
-         <?php else: ?>
-           投稿が無い時の処理
-         <?php endif; ?>
       </div>
   </section>
 <?php get_footer(); ?>
