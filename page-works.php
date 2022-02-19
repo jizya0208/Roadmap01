d9cf1a7fb256fc77f3fedb79b9821ada90b13629
jizya0208 <moridyan606@gmail.com>
2022-02-19 15:55:14 +0900

    mainタグ追加

--- page-works.php
+++ page-works.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-
+<main></main>
 
 
 <?php get_footer(); ?>
--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,6 @@
 jQuery(function () {
    jQuery(window).on('scroll', function () {
-      const targetPosition = jQuery(".l-header").height() + 300;
+      const targetPosition = jQuery(".home>.l-header").height() + 300;
       var currentPosition = jQuery(this).scrollTop();
       if ( targetPosition < currentPosition ) {
          jQuery('.l-header').addClass('change-color');
@@ -8,4 +8,6 @@ jQuery(function () {
          jQuery('.l-header').removeClass('change-color');
      }
    });
+   var height = $('.l-header').outerHeight();
+   $('main').css('padding-top', height);
 });
