17a4a5aee3bd791008c62cbbafdb6ccb911baf40
jizya0208 <moridyan606@gmail.com>
2022-03-06 15:24:41 +0900

    問い合わせフォーム編集

--- page-contact.php
+++ page-contact.php
@@ -11,18 +11,8 @@ Template Name: お問い合わせ
             <h1 class="c-ttl">CONTACT</h1>
             <p class="lead">お仕事のご依頼などお待ちしております。</p>
             <div class="form-box">
-               <?php if (have_posts()): ?>
-                  <?php while (have_posts()) : the_post(); ?>
-                     <?php echo apply_filters(the_content()) ?>
-                     <?php echo apply_filters( 'the_content', get_post(25)->post_content ); ?>
-                  <?php endwhile; ?>
-               <?php else: ?>
-                  投稿が無い時の処理
-               <?php endif; ?>
+               <?= do_shortcode('[mwform_formkey key="27"]') ?>
             </div>
-                  <!-- $post_id = 25;
-                  $post = get_post($post_id);
-                  echo apply_filters( 'the_content', $page->post_content ); -->
          </div>
       </section>
    </main>
